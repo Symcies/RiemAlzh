3816a013ae3f182c12bbd0b5f4bbd1dc633bf385
Symcies <koval.igor.92@gmail.com>
2017-03-06 16:26:59 +0100

    examples with parameters xml

--- src/main.cpp
+++ src/main.cpp
@@ -19,35 +19,35 @@ using namespace std;
 
 #include "Observations.h"
 
-#include "omp.h"
+//#include "omp.h"
 #include "tinyxml2.h"
 
 
 int main(int argc, char* argv[]) {
-  
+
   if(argc != 4)
   {
       std::cerr << "Usage with real data: " << " /path/to/executable " << " model_settings.xml " << " algorithm_settings " << "data_settings.xml" << std::endl;
   }
-    
+
   /// Initialize tests
   TestAssert::Init(false);
-  
+
   /// Load the XML file arguments
   io::ModelSettings     MS(argv[1]);
   io::AlgorithmSettings AS(argv[2]);
   io::DataSettings      DS(argv[3]);
-  
+
   /// Initialize the sampler
   std::shared_ptr<AbstractSampler> Sampler = make_shared<BlockedGibbsSampler>();
-  
+
   /// Initialize the model
   std::shared_ptr<AbstractModel> Model;
   if(MS.GetType() == "Meshwork")     Model = make_shared<MeshworkModel>(MS);
   if(MS.GetType() == "FastNetwork")  Model = make_shared<FastNetworkModel>(MS);
   if(MS.GetType() == "Network")      Model = make_shared<NetworkModel>(MS);
   if(MS.GetType() == "Multivariate") Model = make_shared<MultivariateModel>(MS);
-  
+
   /// Initialize the data
   Observations Obs;
   if(DS.IsReal())
@@ -55,28 +55,28 @@ int main(int argc, char* argv[]) {
     Obs = io::ReadData::ReadObservations(DS);
     Obs.InitializeGlobalAttributes();
   }
-  else 
+  else
   {
     Model->InitializeFakeRandomVariables();
     Obs = Model->SimulateData(DS);
   }
-  
+
   /// Algorithm pipeline
   auto Algo = make_shared<Algorithm>(AS);
   Algo->SetModel(Model);
   Algo->SetSampler(Sampler);
   Algo->ComputeMCMCSAEM(Obs);
-  
-  
+
+
   return 0;
-  
+
 #pragma omp parallel for
   //for(int i = 0; i < 10; ++i)
     //printf("(%d - %d)", i,omp_get_num_threads());
-  
+
   }
-  
-  
+
+
   //
   //                       _oo0oo_
   //                      o8888888o
